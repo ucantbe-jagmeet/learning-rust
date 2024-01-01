d55eae8fb59116a9516f7a2c5bd454e58e3623ee
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-01-01 23:10:41 +0530

    expressions vs statements

--- by-blockchain-wala/main-file/src/main.rs
+++ by-blockchain-wala/main-file/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+    let sum = sum_of_xandy(30, 50);
+    another_function1();
+    println!("Sum of x and y is : {sum}");
+
+}
+
+fn sum_of_xandy(x : u32, y: u32) -> u32{
+    let sum = x+ y;
+    return  sum;
+
+}
+
+fn another_function1(){
+    println!("Another Function");
 }
