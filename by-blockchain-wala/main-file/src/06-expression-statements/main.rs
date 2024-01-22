96ee0a10bafb271998039f3b4d98e34635a877c8
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-01-22 23:51:28 +0530

    expressions vs statements

--- by-blockchain-wala/main-file/src/06-expression-statements/main.rs
+++ by-blockchain-wala/main-file/src/06-expression-statements/main.rs
@@ -1,3 +1,21 @@
-fn main(){
-    println!("Slow them down");
+// statement -> it returns nothing
+// expression -> it returns something
+
+
+
+fn main() {
+    println!("Hello, world!");
+    another_function1();
+    println!("Sum of x and y is : {} and {}",sum_of_xandy(30, 50), sum_of_xandy(20, 100) );
+
+}
+
+fn sum_of_xandy(x : u32, y: u32) -> u32{
+    let sum = x+ y;
+    sum
+
+}
+
+fn another_function1(){
+    println!("Another Function");
 }
