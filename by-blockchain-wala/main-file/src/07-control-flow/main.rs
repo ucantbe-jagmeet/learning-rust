daf0258ea7c5c49cc0f028fd413357d26b172d5d
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-01-23 23:55:06 +0530

    control flow

--- by-blockchain-wala/main-file/src/07-control-flow/main.rs
+++ by-blockchain-wala/main-file/src/07-control-flow/main.rs
@@ -1,3 +1,17 @@
-fn main(){
-    
+
+fn main() {
+    let num = 7;
+
+    if num < 5 {
+        println!("True")
+    } else {
+        println!("false")
+    }
+    let condition = false;
+    let number = if condition { 5 } else {10};
+
+    println!("the value is {number}") 
+
 }
+
+
