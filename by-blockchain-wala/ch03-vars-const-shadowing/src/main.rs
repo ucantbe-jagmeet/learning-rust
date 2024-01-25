ab16fb94e774c85b3e1a784136705d313de3dae2
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-01-25 23:39:30 +0530

    shadowing in rust

--- by-blockchain-wala/ch03-vars-const-shadowing/src/main.rs
+++ by-blockchain-wala/ch03-vars-const-shadowing/src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
+    const THREE_HOURS_IN_SECONDS :u32 = 60 * 60 * 3;
+    
     let x = 5;
     let y = 10;
-    println!("The value of x is : {y} adn y is {}", x);
+    println!("The value of y is : {y} and x is {}", x);
+    
+    {
+        let y = 20; // shadowing aka blockscope 
+        println!("The value of y is : {y} and x is {}", x);
+    }
+    
+    // let mut a = 20; // mutability with mut keyword so that programmer does not change value of this
+    // println!("The value of a is : {a}");
+
+
 }
