56b7672f303caa29299843d60aebcd79a2a97131
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-01-31 00:17:29 +0530

    control flow

--- by-blockchain-wala/main-file/src/07-control-flow/main.rs
+++ by-blockchain-wala/main-file/src/07-control-flow/main.rs
@@ -1,3 +1,4 @@
+// use std::{thread, time};
 
 fn main() {
     let num = 7;
@@ -10,7 +11,44 @@ fn main() {
     let condition = false;
     let number = if condition { 5 } else {10};
 
-    println!("the value is {number}") 
+    println!("the value is {number}");
+
+
+    let mut x =0;
+    loop{
+        println!("again!");
+        x+=1;
+        println!("value of x = {x}");
+        
+        if x>=5 {
+            break
+        }
+        
+        // thread::sleep(time::Duration::from_secs(1))
+    }
+    
+    let mut y =0;
+    while y <5 {
+        println!("again!");
+        y+=1;
+        println!("value of y = {y}");
+
+        // thread::sleep(time::Duration::from_secs(1))
+
+    }
+
+
+    let arr = [10, 20, 30];
+
+    let mut index = 0;
+ 
+    while index < 3{
+        println!("the value is: {}", arr[index]);
+        index+=1;
+    }
+    for element in arr {
+            println!("the value of elem is : {element}");
+    }
 
 }
 
---
+++ by-blockchain-wala/main-file/src/07-control-flow/tempCodeRunnerFile.rs
@@ -0,0 +1,7 @@
+ let mut index = 0;
+
+    while index < 5{
+        println!("the value is: {}", arr[index]);
+        index+=1;
+    }
+    
