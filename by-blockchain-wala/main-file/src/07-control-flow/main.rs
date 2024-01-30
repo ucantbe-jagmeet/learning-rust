e963021f9feda0b296a3f9182210ab124717929a
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-01-31 00:02:49 +0530

    if else

--- by-blockchain-wala/main-file/src/07-control-flow/main.rs
+++ by-blockchain-wala/main-file/src/07-control-flow/main.rs
@@ -1,12 +1,12 @@
 
 fn main() {
     let num = 7;
-
     if num < 5 {
         println!("True")
     } else {
         println!("false")
     }
+
     let condition = false;
     let number = if condition { 5 } else {10};
 
