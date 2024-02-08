b1c1698591e53aca1e328c93b60fd935455da57a
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-08 13:21:32 +0530

    mutable reference type

--- by-blockchain-wala/main-file/src/10-references-and-borrowing/main.rs
+++ by-blockchain-wala/main-file/src/10-references-and-borrowing/main.rs
@@ -1,10 +1,12 @@
 fn main(){
-    let s1 = String::from("hello");
-    let len = calculate_len(&s1); // & reference sign means borrow 
+    let mut s1 = String::from("hello");
+    let len = calculate_len(&mut s1); // & sign means borrow/reference : means it doesnot giving its ownership 
+    // there are some rules for borrowing 
 
     println!("The len of {} is {}", s1, len);
 }
 
-fn calculate_len(s: &String)-> usize{
+fn calculate_len(s: &mut String)-> usize{
+    s.push_str(", world");
     s.len()
 }
