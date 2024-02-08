e62eb6a2de33bb0bf284366201830b42eb3eb437
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-08 23:20:18 +0530

    struct in rust

--- by-blockchain-wala/main-file/src/12-struct/main.rs
+++ by-blockchain-wala/main-file/src/12-struct/main.rs
@@ -1,3 +1,18 @@
 fn main(){
-    println!("Struct in rust");
+    let jagmeet = User {
+        active: true,
+        username: String ::from("jagmeet_singh"),
+        email: String ::from("jagmeet@mail.com"),
+        mobile_no: 8989898989
+    };
+    println!("{:#?}", jagmeet)
+}
+
+// custom data types - defined by user
+#[derive(Debug)]
+struct User{
+    active:bool,
+    username: String,
+    email: String,
+    mobile_no: u64,
 }
