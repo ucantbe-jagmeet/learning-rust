91b8f14a69d2faabb66d6f4fec7ea9a47f993d6b
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-08 22:47:05 +0530

    slice example

--- by-blockchain-wala/main-file/src/11-string-and-slices/main.rs
+++ by-blockchain-wala/main-file/src/11-string-and-slices/main.rs
@@ -1,16 +1,27 @@
 fn main(){
-    let mut s: String = String::from("hello world");
-    let x = first_word(&s); // x will get the value of 5    
-    s.clear();
-
-    println!("{}", x); // x still shows 5 which is odd behaviour
-
+    let mut s: String = String::from("hello world this is slice type");
+    // let x = first_word(&s); // x will get the value of 5    
+    // s.clear();
+    
+    // println!("{}", x); // x still shows 5 which is odd behaviour
+    
+    //  ############### using slice ###############
+    
+    // let hello = &s[0..5];
+    // let world = &s[6..11];
+    
+    // println!("{world}")
+    
+    let first = first_word(&s);
+    
+    // s.clear(); // now this gives error that "mutable borrow occurs here" , this is the correct way 
+    println!{"first World{} is: {}", s, first}
 
 }
 
 // usize depend on system for example 32 bit , 64 bit
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
     // println!("{:?}", bytes); // :? is required to print bytes 
 
@@ -18,8 +29,10 @@ fn first_word(s: &String) -> usize {
 
     for ( i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            // return i;
+            return &s[0..i]
         }
     }
-    s.len()
+    &s[.. s.len()]
+    
 }
