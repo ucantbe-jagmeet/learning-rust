3c2c35e62cd8a81565f0fe205ad7adca2b851cb5
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-09 00:02:32 +0530

    struct exaple

--- by-blockchain-wala/main-file/src/12-struct/tempCodeRunnerFile.rs
+++
@@ -1,6 +0,0 @@
-let jagmeet = User {
-    //     active: true,
-    //     username: String ::from("jagmeet_singh"),
-    //     email: String ::from("jagmeet@mail.com"),
-    //     sign_in_count: 9999
-    // };
