a1ba0d5ce8df003250e74daab3989f661661c9e6
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-08 13:46:39 +0530

    mutable nd immut double borrowing

--- by-blockchain-wala/main-file/src/10-references-and-borrowing/main.rs
+++ by-blockchain-wala/main-file/src/10-references-and-borrowing/main.rs
@@ -1,12 +1,32 @@
+// there are some rules for borrowing 
+// -> At any given time , you can have either one mut reference or any number of immutable reference
+// -> References must always be valid
+
+
 fn main(){
     let mut s1 = String::from("hello");
+    print_hellow(&mut s1);
     let len = calculate_len(&mut s1); // & sign means borrow/reference : means it doesnot giving its ownership 
-    // there are some rules for borrowing 
-
     println!("The len of {} is {}", s1, len);
+    
+    // if one variable is mutable reference in single scope then it will not provide mut or immut reference in same scope 
+    // for eg
+    
+    // let s = &mut s1;
+    // let s2 = &mut s1; // this will give doubel mut borrowing error
+    // let s2 = &s1; // this will give immut borrow occurs error
+    // let len = calculate_len(&mut s1, &mut s1);  
+
+    // println!("The len of {} is {}", s1, len);
 }
 
+// fn calculate_len(s: &mut String, p : &mut String)-> usize{
 fn calculate_len(s: &mut String)-> usize{
     s.push_str(", world");
     s.len()
+}
+
+fn print_hellow(s: &mut String){
+    s.push_str(", ok");
+    println!("{}", s);
 }
