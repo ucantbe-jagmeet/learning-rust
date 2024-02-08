8c670c8f9cefeba4366c82030b2098981e974fd0
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-09 01:32:06 +0530

    Self keyword as a output

--- by-blockchain-wala/main-file/src/14-methods-function/main.rs
+++ by-blockchain-wala/main-file/src/14-methods-function/main.rs
@@ -6,8 +6,26 @@ struct Rectangle {
 }
 
 impl Rectangle{
+    // the first parameter of function in methods will be always secure for self
+    // (self : Self )
     fn area(&self) -> u32{
         self.width * self.height
+    }
+
+    fn peri(self : &Self) -> u32{
+        2 * (self.width + self.height)
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+
+    // returning output as a self
+    fn square(size : u32) -> Self {
+        Self {
+            width : size, 
+            height : size 
+        }
     }
 }
 
@@ -18,9 +36,28 @@ fn main(){
         height: 35
     };
 
+    let rect2 = Rectangle{
+        width: 12,
+        height: 34
+    };
+
     println!(
         "The area of rectangle is : {}",
         rect1.area()
-    )
+    );
+    println!(
+        "The perimeter of rectangle is : {}",
+        rect1.peri()
+    );
+    println!(
+        "can hold rect2 ? {}",
+        rect1.can_hold(&rect2)
+    );
 
+    let new_square = Rectangle::square(10);
+
+    println!(
+        "Area of square is: {}",
+        new_square.area()
+    )
 }
