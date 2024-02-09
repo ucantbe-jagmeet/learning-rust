96b4c6bb2b96a6e10b6217cab603ab7e608fdecf
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-10 01:51:01 +0530

    Error example non recoverable

--- by-blockchain-wala/main-file/src/19-error-handling/main.rs
+++ by-blockchain-wala/main-file/src/19-error-handling/main.rs
@@ -1,3 +1,36 @@
+// Rust group Error divided into two major categories
+// -> Recoverable := such as file not found error, we most likely just want to report the problem to the user and retry the operation
+// -> Unrecoverable := those errors are always symptoms of bugs, like trying to access a location beyond the end of an array, and so we want to immediately stop the program
+
+// Result<T, E> type is use for recoverable errors
+/*
+enum Result < T, E> {
+        Ok(T),
+        Err(E),
+    }
+
+*/
+// panic! type is use for unrecoverable errors
+
+/*
+    panic -> By default when a panic occurs, the program starts unwinding, which means rust walks back up the stack and cleans up the data from each function it ecounters. However, this walking back and clean up is alot of work . Rust , therefore , allows you to choose the alternative of immediately aborting, which ends the program without cleaning up.
+*/
+use std::fs::File;
+
+pub enum Result < T, E> {
+        Ok(T),
+        Err(E),
+    }
 fn main(){
-    println!("Error handling in rust");
+    // panic!("crash and burn");
+
+    // let v = vec![ 1, 2, 3];
+    // v[99];
+
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:#?}", error),
+    };
 }
