6ad6e9d3fd84a56d96dce08c49db0c36c2f85a9a
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-10 00:17:36 +0530

    example using if else loop

--- by-blockchain-wala/main-file/src/18-control-flow-if-let/main.rs
+++ by-blockchain-wala/main-file/src/18-control-flow-if-let/main.rs
@@ -1,6 +1,13 @@
+#[derive(PartialEq)]
+enum Animal {
+    Dog,
+    Cat,
+    Bird
+}
+
 fn main(){
     // println!("control flow with if let");
-    
+
     let config_max = Some(3u32);
 
     match config_max {
@@ -12,4 +19,24 @@ fn main(){
     if let Some(max) = config_max {
         println!("The maximum is configured to be {}", max);
     }
+
+    let animal = Animal::Cat;
+    IfElse(animal);
+
+}
+
+fn IfElse(animal : Animal) {
+    if animal == Animal::Dog {
+        println!("It's a dog!");
+    }
+    else if animal == Animal::Cat {
+        println!("It's a Cat!");
+    }
+    else if animal == Animal::Bird {
+        println!("It's a Bird!");
+    }
+    else {
+        println!("Unknown Creature");
+    }
+    
 }
