ff17b3b3727ede5753c57343ce19d45ad2577f14
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-09 21:33:33 +0530

    match using Shape example

--- by-blockchain-wala/main-file/src/17-match/main.rs
+++ by-blockchain-wala/main-file/src/17-match/main.rs
@@ -4,12 +4,17 @@ enum Coin{
     Dime,
     Quarter
 }
+enum Shape{
+    Circle( f64),
+    Rectangle( f64, f64),
+    Triangle( f64, f64, f64)
+}
 
 fn value_in_cents( coin: Coin) -> u8 {
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 5,
-        Coin::Dime => { 10},
+        Coin::Dime => 10,
         Coin::Quarter => 25
     }
 }
@@ -26,7 +31,27 @@ fn main(){
     }
 
 
-    let xyz = Coin::Dime;
+    let xyz = Coin::Nickel;
     let value = value_in_cents(xyz);
     println!("value is {}", value);
+
+
+    // ####### Shape Example #######
+    let shape = Shape::Circle( 144.0);
+
+    match shape {
+        Shape :: Circle(radius) => {
+            let area = 3.14 * radius * radius;
+            println!("It's a circle with radius {} and {}", radius, area);
+        },
+        Shape :: Rectangle( width, height ) => {
+            println!("It's a Rectangle whose width and height are {} and {}", width, height);
+        },
+        Shape :: Triangle(s1, s2, s3) => {
+            println!("It's a Triangle whose sides are {}, {} and {}", s1, s2, s3);
+        }
+    }
+
+
+
 }
