172d36b86992f27a93f8c21ecc7c303c5e65c0f4
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-10 00:20:26 +0530

    traits in rust

--- by-blockchain-wala/main-file/src/21-traits/main.rs
+++ by-blockchain-wala/main-file/src/21-traits/main.rs
@@ -22,6 +22,12 @@ pub struct Tweet{
     pub retweet: bool,
 }
 
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format("{}: {}", self.username, self.content)
+    }
+}
+
 fn main(){
 
 }
