445ad0b1a950ee29c8de1cef8526bedea378380c
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-14 22:29:43 +0530

    declerative macros

--- by-blockchain-wala/main-file/src/23-rust-macros/declerarive-macros.rs
+++ by-blockchain-wala/main-file/src/23-rust-macros/declerarive-macros.rs
@@ -1,3 +1,5 @@
+// $expr are macro fragments specifiers
+// Fragment specifiers are placeholders that are used to refer to specific parts of the input to the macro
 macro_rules! my_rule{
     ($arg1:expr) => {};
 }
