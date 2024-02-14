526259783f391622f12e6e6f7fc2c4e0dba7335b
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-14 23:18:48 +0530

    other examples

--- by-blockchain-wala/decleartive-macros/src/lib.rs
+++ by-blockchain-wala/decleartive-macros/src/lib.rs
@@ -1,5 +1,9 @@
 // $expr are macro fragments specifiers
 // Fragment specifiers are placeholders that are used to refer to specific parts of the input to the macro
+#[macro_export]
+macro_rules! my_rule {
+    ($arg1:ty , $arg2:expr, $arg3:path) => {};
+}
 
 #[macro_export]
 macro_rules! makevec {
@@ -20,3 +24,16 @@ fn empty_vec() {
     assert!(x.is_empty());
 }
 
+#[test]
+fn single() {
+    let mut x = makevec![];
+    x.push(4);
+    assert_eq!(x.len(), 1);
+}
+
+#[test]
+fn new_func(){
+    let x= makevec![50];
+    assert_eq!(x.len(), 1);
+    assert_eq!(x[0], 50);
+}
