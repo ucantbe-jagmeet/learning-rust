544b72dbdd5d5327ba9d5aba3fcd13fe0da70d8a
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-14 23:18:29 +0530

    make vector

--- by-blockchain-wala/decleartive-macros/src/lib.rs
+++ by-blockchain-wala/decleartive-macros/src/lib.rs
@@ -1,14 +1,22 @@
 // $expr are macro fragments specifiers
 // Fragment specifiers are placeholders that are used to refer to specific parts of the input to the macro
-macro_rules! my_rule {
-    ($arg1:expr) => {{
-            let x = $arg1; 
-            x
-    }};
+
+#[macro_export]
+macro_rules! makevec {
+    () => {
+        Vec::new()
+    };
+    ($element:expr) => {
+
+      {  let mut x = Vec::new();
+        x.push($element);
+        x}
+    };
 }
 
 #[test]
-fn check() {
-    let val = my_rule!(3);
-    assert_eq!(val,3);
+fn empty_vec() {
+    let x: Vec<u32> = makevec!();
+    assert!(x.is_empty());
 }
+
