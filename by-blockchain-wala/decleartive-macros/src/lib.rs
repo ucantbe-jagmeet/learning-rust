f413f0adf59804d83a84be03755ca94cf930dfeb
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-14 22:56:53 +0530

    adding expression

--- by-blockchain-wala/decleartive-macros/src/lib.rs
+++ by-blockchain-wala/decleartive-macros/src/lib.rs
@@ -1,14 +1,14 @@
 // $expr are macro fragments specifiers
 // Fragment specifiers are placeholders that are used to refer to specific parts of the input to the macro
-macro_rules! my_rule{
-    ($arg1:expr) => {
-        let x = arg1;
-        x
-    };
+macro_rules! my_rule {
+    ($arg1:expr) => {{
+            let x = $arg1; 
+            x
+    }};
 }
 
 #[test]
-fn check (){
+fn check() {
     let val = my_rule!(3);
-    assert_eq!(val, 3);
+    assert_eq!(val,3);
 }
