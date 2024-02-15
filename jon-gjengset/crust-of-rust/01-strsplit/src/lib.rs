1f7389de2547154e3566bc9fb0241721e381d389
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-16 02:14:52 +0530

    structs and methods

--- jon-gjengset/crust-of-rust/01-strsplit/src/lib.rs
+++ jon-gjengset/crust-of-rust/01-strsplit/src/lib.rs
@@ -1,9 +1,17 @@
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
-pub struct StrSplit {}
+pub struct StrSplit {
+    remainder: &str,
+    delimiter: &str,
+}
 
 impl StrSplit {
-    pub fn new( haystack: &str, delimiter: &str) -> Self {}
+    pub fn new(haystack: &str, delimiter: &str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
 }
 
 // let x : StrSplit;
@@ -11,7 +19,19 @@ impl StrSplit {
 
 impl Iterator for StrSplit {
     type Item = &str;
-    fn next(&mut self) -> Option<Self::Item> {}
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter){
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[ (next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } self.remainder.is_empty()_{
+            None
+        } else_{
+            let rest = self.remainder;
+            self.remainder = &[];
+            Some(rest)
+        }
+    }
 }
 
 #[test]
@@ -19,5 +39,6 @@ fn it_works(){
     let haystack = "a b c d e";
     let letters =  StrSplit::new(haystack," ");
 
-    assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
+    let result = letters.collect();
+    assert_eq!(result, vec!["a", "b", "c", "d", "e"].into_iter());
 }
