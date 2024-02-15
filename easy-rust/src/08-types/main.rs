e8cf082caeeab6f5a1d3f2a49faa4cebdfe89d8e
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-16 02:33:24 +0530

    types in rust

---
+++ easy-rust/src/08-types/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let first_letter = 'A';
+    let space = ' '; // A space inside ' ' is also a char
+    let other_language_char = 'Ꮔ'; // Thanks to Unicode, other languages like Cherokee display just fine too
+    let cat_face = '😺'; // Emojis are chars too
+
+     let my_number :u8= 100; // We didn't write a type of integer,
+                         // so Rust chooses i32. Rust always
+                         // chooses i32 for integers if you don't
+                         // tell it to use a different type
+
+    println!("{}", my_number as u8 as char); 
+    
+    println!("Size of a char: {}", std::mem::size_of::<char>()); // 4 bytes
+    println!("Size of string containing 'a': {}", "a".len()); // .len() gives the size of the string in bytes
+    println!("Size of string containing 'ß': {}", "ß".len());
+    println!("Size of string containing '国': {}", "国".len());
+    println!("Size of string containing '𓅱': {}", "𓅱".len());
+}
