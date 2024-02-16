c295dcdb478d191b84c870c3420f6e8f56364edb
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-17 00:29:05 +0530

    more printing methods

--- easy-rust/src/14-more-about-printing/main.rs
+++ easy-rust/src/14-more-about-printing/main.rs
@@ -28,4 +28,22 @@ fn main() {
     // {:b} binary
     // {:x} hexadecimal
     // {:o} octal
+
+    let letter = "a";
+    println!("{:ㅎ^11}", letter); // this will print ㅎㅎㅎㅎㅎaㅎㅎㅎㅎㅎ
+    
+     let title = "TODAY'S NEWS";
+    println!("{:-^30}", title); // no variable name, pad with -, put in centre, 30 characters long
+    let bar = "|";
+    println!("{: <15}{: >15}", bar, bar); // no variable name, pad with space, 15 characters each, one to the left, one to the right
+    let a = "SEOUL";
+    let b = "TOKYO";
+    println!("{city1:-<15}{city2:->15}", city1 = a, city2 = b); // variable names city1 and city2, pad with -, one to the left, one to the right
+
+    /*
+        ---------TODAY'S NEWS---------
+        |                            |
+        SEOUL--------------------TOKYO
+    
+     */
 }
