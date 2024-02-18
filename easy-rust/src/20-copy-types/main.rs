46a6d227bb1dac5edcdf4e44926ee3000c56cf94
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-18 23:45:20 +0530

    copy types

--- easy-rust/src/20-copy-types/main.rs
+++ easy-rust/src/20-copy-types/main.rs
@@ -5,10 +5,16 @@ fn prints_number(number: i32) { // There is no -> so it's not returning anything
                              // and we couldn't use it again
     println!("{}", number);
 }
-
+fn prints_country(country_name: String) {
+    println!("{}", country_name);
+}
 fn main() {
     let my_number = 8;
     prints_number(my_number); // Prints 8. prints_number gets a copy of my_number
     prints_number(my_number); // Prints 8 again.
                               // No problem, because my_number is copy type!
+
+    // let country = String::from("Kiribati");
+    // prints_country(country);
+    // prints_country(country); // this will give error
 }
