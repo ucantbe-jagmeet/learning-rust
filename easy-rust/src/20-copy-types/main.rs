134b46475f98e92751cce2fd8c5f9537f492be86
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-18 23:48:35 +0530

    clone string

--- easy-rust/src/20-copy-types/main.rs
+++ easy-rust/src/20-copy-types/main.rs
@@ -8,6 +8,11 @@ fn prints_number(number: i32) { // There is no -> so it's not returning anything
 fn prints_country(country_name: String) {
     println!("{}", country_name);
 }
+
+fn get_length(input: String) { // Takes ownership of a String
+    println!("It's {} words long.", input.split_whitespace().count()); // splits to count the number of words
+}
+
 fn main() {
     let my_number = 8;
     prints_number(my_number); // Prints 8. prints_number gets a copy of my_number
@@ -18,4 +23,10 @@ fn main() {
     // prints_country(country); // we have to clone it first
     // prints_country(country.clone()); 
     // prints_country(country); // this will give error
+
+    let mut my_string = String::new();
+    for _ in 0..50 {
+        my_string.push_str("Here are some more words"); // push the words on
+        get_length(my_string.clone()); // gives it a clone every time
+    }
 }
