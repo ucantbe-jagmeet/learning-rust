0312e44aa18615fe97dea45da97adf92583d0b98
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-18 22:42:06 +0530

    giving reference to functions

--- easy-rust/src/19-ref-to-func/main.rs
+++ easy-rust/src/19-ref-to-func/main.rs
@@ -7,6 +7,19 @@ fn print_country1(country_name: String) -> String {
     country_name // return it here
 }
 
+fn print_ref_country(country_name: &String) {
+    println!("{}", country_name);
+}
+
+fn add_hungary(country_name: &mut String) { // first we say that the function takes a mutable reference
+    country_name.push_str("-Hungary"); // push_str() adds a &str to a String
+    println!("Now it says: {}", country_name);
+}
+
+fn mut_hungary(mut country: String) { // Here's how: adds_hungary takes the String and declares it mutable!
+    country.push_str("-Hungary");
+    println!("{}", country);
+}
 fn main() {
     let country = String::from("Austria");
     print_country(country); // We print "Austria"
@@ -14,4 +27,21 @@ fn main() {
     
     let country = print_country(country);
     print_country1(country); 
+
+    print_ref_country(&country); // We print "Austria"
+
+    let mut country = String::from("Austria");
+    add_hungary(&mut country); 
+
+    mut_hungary(country);
 }
+
+
+/*
+    So to conclude:
+
+    fn function_name(variable: String) takes a String and owns it. If it doesn't return anything, then the variable dies inside the function.
+    fn function_name(variable: &String) borrows a String and can look at it
+    fn function_name(variable: &mut String) borrows a String and can change it
+
+*/
