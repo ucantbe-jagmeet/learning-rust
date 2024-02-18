211e27992149bce3c09baa0d541e61926c5c23ba
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-18 22:24:09 +0530

    more about reference

--- easy-rust/src/18-main-reference/main.rs
+++ easy-rust/src/18-main-reference/main.rs
@@ -7,4 +7,20 @@ fn main() {
     let second_number = 800;
     let triple_reference = &&&second_number;
     println!("Second_number = triple_reference? {}", second_number == ***triple_reference);
+
+    // ##################################
+    let mut number = 10;
+    let number_change = &mut number; // create a mutable reference
+    *number_change += 15; // use mutable reference to add 10
+    let number_ref = &number; // create an immutable reference
+    println!("{}", number_ref); // print the immutable reference
+
+    // ###########################################
+
+    
+    let country = String::from("Austria"); // Now we have a String called country
+    let country_ref = &country; // country_ref is a reference to this data. It's not going to change
+    let country = 8; // Now we have a variable called country that is an i8. But it has no relation to the other one, or to country_ref
+    println!("{}, {}", country_ref, country); // country_ref still refers to the data of String::from("Austria") that we gave it.
+
 }
