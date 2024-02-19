28d56fef1562e7c96e34f555ab7b54737f637441
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-20 01:10:24 +0530

    structs

---
+++ easy-rust/src/25-structs/main.rs
@@ -0,0 +1,6 @@
+struct Colour(u8, u8, u8);
+
+fn main() {
+    let my_colour = Colour(50, 0, 50); // Make a colour out of RGB (red, green, blue)
+    println!("The second part of the colour is: {}", my_colour.1);
+}
