7dd3bd7c7ae0fdbe8d044e6cc941e9176e803a97
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-21 00:57:46 +0530

    enums with strings

--- easy-rust/src/26-enums/main.rs
+++ easy-rust/src/26-enums/main.rs
@@ -2,6 +2,36 @@ enum ThingsInTheSky {
     Sun(String), // Now each variant has a string
     Stars(String),
 }
+enum Mood {
+    Happy,
+    Sleepy,
+    NotBad,
+    Angry,
+}
+enum Season {
+    Spring, // If this was Spring(String) or something it wouldn't work
+    Summer,
+    Autumn,
+    Winter,
+}
+enum Star {
+    BrownDwarf = 10,
+    RedDwarf = 50,
+    YellowStar = 100,
+    RedGiant = 1000,
+    DeadStar, // Think about this one. What number will it have?
+}
+
+fn match_mood(mood: &Mood) -> i32 {
+    use Mood::*;
+    let happiness_level = match mood {
+        Happy => 10, // Here we type Mood:: every time
+        Sleepy => 6,
+        NotBad => 7,
+        Angry => 2,
+    };
+    happiness_level
+}
 
 fn create_skystate(time: i32) -> ThingsInTheSky {
     match time {
@@ -21,4 +51,25 @@ fn main() {
     let time = 8; // it's 8 o'clock
     let skystate = create_skystate(time); // create_skystate returns a ThingsInTheSky
     check_skystate(&skystate); // Give it a reference so it can read the variable skystate
+
+    let my_mood = Mood::NotBad;
+    let happiness_level = match_mood(&my_mood);
+    println!("Out of 1 to 10, my happiness is {}", happiness_level);
+
+    use Season::*;
+    let four_seasons = vec![Spring, Summer, Autumn, Winter];
+    for season in four_seasons {
+        println!("{}", season as u32);
+    }
+
+    use Star::*;
+    let starvec = vec![BrownDwarf, RedDwarf, YellowStar, RedGiant];
+    for star in starvec {
+        match star as u32 {
+            size if size <= 80 => println!("Not the biggest star."), // Remember: size doesn't mean anything. It's just a name we chose so we can print it
+            size if size >= 80 => println!("This is a good-sized star."),
+            _ => println!("That star is pretty big!"),
+        }
+    }
+    println!("What about DeadStar? It's the number {}.", DeadStar as u32);
 }
