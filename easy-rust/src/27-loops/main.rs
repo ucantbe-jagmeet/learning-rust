fad6eca1055eb68fc4abbd97bd9fa351364b6afa
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-21 01:39:49 +0530

    loops in rust

--- easy-rust/src/27-loops/main.rs
+++ easy-rust/src/27-loops/main.rs
@@ -1,5 +1,25 @@
 // If you have a loop inside of a loop, you can give them names. With names, you can tell Rust which loop to break out of. Use ' (called a "tick") and a : to give it a name:
 
+fn match_colours(rbg: (i32, i32, i32)) {
+    println!(
+        "Comparing a colour with {} red, {} blue, and {} green:",
+        rbg.0, rbg.1, rbg.2
+    );
+    let new_vec = vec![(rbg.0, "red"), (rbg.1, "blue"), (rbg.2, "green")]; // Put the colours in a vec. Inside are tuples with the colour names
+    let mut all_have_at_least_10 = true; // Start with true. We will set it to false if one colour is less than 10
+    for item in new_vec {
+        if item.0 < 10 {
+            all_have_at_least_10 = false; // Now it's false
+            println!("Not much {}.", item.1) // And we print the colour name.
+        }
+    }
+    if all_have_at_least_10 {
+        // Check if it's still true, and print if true
+        println!("Each colour has at least 10.")
+    }
+    println!(); // Add one more line
+}
+
 fn main() {
     let mut counter = 0;
     let mut counter2 = 0;
@@ -23,4 +43,38 @@ fn main() {
             }
         }
     }
+    // while loop in rust
+    while counter < 10 {
+        counter += 1;
+        println!("The counter is now: {}", counter);
+    }
+
+    for number in 0..3 {
+        println!("The number is: {}", number);
+    }
+
+    for number in 0..=3 {
+        println!("The next number is: {}", number);
+    }
+    for _ in 0..3 {
+        println!("Printing the same thing three times"); // if you didnt use numbers in your program, then rust will give you reminder
+    }
+
+    let mut counter1 = 5;
+    // returning value from a loop
+    let my_number = loop {
+        counter += 1;
+        if counter % 53 == 3 {
+            break counter; // it means break and return the value of counter
+        }
+    };
+    println!("{}", my_number);
+
+    let first = (200, 0, 0);
+    let second = (50, 50, 50);
+    let third = (200, 50, 0);
+
+    match_colours(first);
+    match_colours(second);
+    match_colours(third);
 }
