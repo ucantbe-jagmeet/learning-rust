11f24cc929d06299319c1af79f471ca6cd333a8a
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-20 23:39:18 +0530

    structs

--- easy-rust/src/25-structs/main.rs
+++ easy-rust/src/25-structs/main.rs
@@ -1,7 +1,12 @@
 struct Colour(u8, u8, u8);
 struct SizeAndColour {
-    size: u32,
+    size: u32,      //  No comma here gives gives error
     colour: Colour, // And we put it in our new named struct
+}
+struct Country {
+    population: u32,
+    capital: String,
+    leader_name: String,
 }
 fn main() {
     let my_colour = Colour(50, 0, 50); // Make a colour out of RGB (red, green, blue)
@@ -11,4 +16,13 @@ fn main() {
         colour: my_colour,
     };
     println!("The second part of the colour is: {}", my_colour.1);
+    let population = 500_000;
+    let capital = String::from("Elista");
+    let leader_name = String::from("Batu Khasikov");
+
+    let kalmykia = Country {
+        population: population,
+        capital: capital,
+        leader_name: leader_name,
+    };
 }
