3e76ba5d0199a15be8cfd9044eab2756dbea2c0f
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-21 02:43:20 +0530

    reference number and dot operator

--- easy-rust/src/30-reference-dot-operator/tempCodeRunnerFile.rs
+++
@@ -1,8 +0,0 @@
-// references and dot operators
-
-fn main() {
-    let my_number = 10;
-    let reference_number = &my_number;
-
-    println!("The value of my_number is {}", my_number);
-    println!("{}", my_number == reference
