9e4b5f590ccd87b65955bf01e4e8d20b2a949337
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-20 22:35:05 +0530

    names struct

--- easy-rust/src/25-structs/main.rs
+++ easy-rust/src/25-structs/main.rs
@@ -1,6 +1,14 @@
 struct Colour(u8, u8, u8);
-
+struct SizeAndColour {
+    size: u32,
+    colour: Colour, // And we put it in our new named struct
+}
 fn main() {
     let my_colour = Colour(50, 0, 50); // Make a colour out of RGB (red, green, blue)
+
+    let size_and_colour = SizeAndColour {
+        size: 150,
+        colour: my_colour,
+    };
     println!("The second part of the colour is: {}", my_colour.1);
 }
