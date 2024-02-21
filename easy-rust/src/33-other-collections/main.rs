d1ba59c7e108df14666708b8a69c9c1628f48780
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-22 01:02:46 +0530

    vector deque

--- easy-rust/src/33-other-collections/main.rs
+++ easy-rust/src/33-other-collections/main.rs
@@ -8,7 +8,7 @@
 
 // You often use a HashSet if you just want to know if a key exists, or doesn't exist.
 use std::collections::hash_map::*;
-use std::collections::{BTreeMap, BinaryHeap, HashMap, HashSet};
+use std::collections::{BTreeMap, BinaryHeap, HashMap, HashSet, VecDeque};
 
 struct City {
     name: String,
@@ -22,6 +22,20 @@ fn show_remainder(input: &BinaryHeap<i32>) -> Vec<i32> {
         remainder_vec.push(*number)
     }
     remainder_vec
+}
+fn check_remaining(input: &VecDeque<(&str, bool)>) {
+    // Each item is a (&str, bool)
+    for item in input {
+        if item.1 == false {
+            println!("You must: {}", item.0);
+        }
+    }
+}
+
+fn done(input: &mut VecDeque<(&str, bool)>) {
+    let mut task_done = input.pop_back().unwrap(); // pop off the back
+    task_done.1 = true; // now it's done - mark as true
+    input.push_front(task_done); // put it at the front now
 }
 fn main() {
     // let mut tallinn = City {
@@ -224,4 +238,30 @@ fn main() {
     // while let Some(job) = jobs.pop() {
     //     println!("You need to: {}", job.1);
     // }
+
+    //###################### vecDeque ########################
+    let mut my_vec = VecDeque::from(vec![0; 600000]);
+    for i in 0..600000 {
+        my_vec.pop_front(); // pop_front is like .pop but for the front
+    }
+
+    let mut my_vecdeque = VecDeque::new();
+    let things_to_do = vec![
+        "send email to customer",
+        "add new product to list",
+        "phone Loki back",
+    ];
+
+    for thing in things_to_do {
+        my_vecdeque.push_front((thing, false));
+    }
+
+    done(&mut my_vecdeque);
+    done(&mut my_vecdeque);
+
+    check_remaining(&my_vecdeque);
+
+    for task in my_vecdeque {
+        print!("{:?} ", task);
+    }
 }
