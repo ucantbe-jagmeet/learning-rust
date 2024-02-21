206f3036d883ab6ff4b83523a56e6e2f2a6e3a89
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-21 23:32:25 +0530

    tuple in hashmap

--- easy-rust/src/33-other-collections/main.rs
+++ easy-rust/src/33-other-collections/main.rs
@@ -105,21 +105,43 @@ fn main() {
         }
         */
 
-    let book_collection = vec![
-        "L'Allemagne Moderne",
-        "Le Petit Prince",
-        "Eye of the World",
-        "Eye of the World",
+    // let book_collection = vec![
+    //     "L'Allemagne Moderne",
+    //     "Le Petit Prince",
+    //     "Eye of the World",
+    //     "Eye of the World",
+    // ];
+
+    // let mut book_hashmap = HashMap::new();
+
+    // for book in book_collection {
+    //     let return_value = book_hashmap.entry(book).or_insert(0); // return_value is a mutable reference. If nothing is there, it will be 0
+    //     *return_value += 1; // Now return_value is at least 1. And if there was another book, it will go up by 1
+    // }
+
+    // for book in book_hashmap {
+    //     println!("{:?}", book);
+    // }
+
+    //#################### use of entry #############################
+    let data = vec![
+        // This is the raw data
+        ("male", 9),
+        ("female", 5),
+        ("male", 0),
+        ("female", 6),
+        ("female", 5),
+        ("male", 10),
     ];
 
-    let mut book_hashmap = HashMap::new();
+    let mut survey_hash = HashMap::new();
 
-    for book in book_collection {
-        let return_value = book_hashmap.entry(book).or_insert(0); // return_value is a mutable reference. If nothing is there, it will be 0
-        *return_value += 1; // Now return_value is at least 1. And if there was another book, it will go up by 1
+    for item in data {
+        // This gives a tuple of (&str, i32)
+        survey_hash.entry(item.0).or_insert(Vec::new()).push(item.1); // This pushes the number into the Vec inside
     }
 
-    for book in book_hashmap {
-        println!("{:?}", book);
+    for (male_or_female, numbers) in survey_hash {
+        println!("{:?}: {:?}", male_or_female, numbers);
     }
 }
