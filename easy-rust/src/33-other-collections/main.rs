9b86ba86ec797a3844556162708915973489a492
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-22 00:29:55 +0530

    binary heap

--- easy-rust/src/33-other-collections/main.rs
+++ easy-rust/src/33-other-collections/main.rs
@@ -195,20 +195,33 @@ fn main() {
      */
 
     // ################## Binary heap #########################
-    let many_numbers = vec![0, 5, 10, 15, 20, 25, 30]; // These numbers are in order
+    // let many_numbers = vec![0, 5, 10, 15, 20, 25, 30]; // These numbers are in order
 
-    let mut my_heap = BinaryHeap::new();
+    // let mut my_heap = BinaryHeap::new();
 
-    for number in many_numbers {
-        my_heap.push(number);
-    }
+    // for number in many_numbers {
+    //     my_heap.push(number);
+    // }
 
-    while let Some(number) = my_heap.pop() {
-        // .pop() returns Some(number) if a number is there, None if not. It pops from the front
-        println!(
-            "Popped off {}. Remaining numbers are: {:?}",
-            number,
-            show_remainder(&my_heap)
-        );
-    }
+    // while let Some(number) = my_heap.pop() {
+    //     // .pop() returns Some(number) if a number is there, None if not. It pops from the front
+    //     println!(
+    //         "Popped off {}. Remaining numbers are: {:?}",
+    //         number,
+    //         show_remainder(&my_heap)
+    //     );
+    // }
+
+    // let mut jobs = BinaryHeap::new();
+
+    // // Add jobs to do throughout the day
+    // jobs.push((100, "Write back to email from the CEO"));
+    // jobs.push((80, "Finish the report today"));
+    // jobs.push((5, "Watch some YouTube"));
+    // jobs.push((70, "Tell your team members thanks for always working hard"));
+    // jobs.push((30, "Plan who to hire next for the team"));
+
+    // while let Some(job) = jobs.pop() {
+    //     println!("You need to: {}", job.1);
+    // }
 }
