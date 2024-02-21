918c80b49172cabef20f0337d5f5d37475b5ad46
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-22 01:38:21 +0530

    assert

--- easy-rust/src/34-question-operator/main.rs
+++ easy-rust/src/34-question-operator/main.rs
@@ -1,3 +1,13 @@
+/*
+There are three other macros that are similar to panic! that you use a lot in testing. They are: assert!, assert_eq!, and assert_ne!.
+
+Here is what they mean:
+
+assert!(): if the part inside () is not true, the program will panic.
+assert_eq!(): the two items inside () must be equal.
+assert_ne!(): the two items inside () must not be equal. (ne means not equal)
+*/
+
 use std::num::ParseIntError;
 
 fn parse_str(input: &str) -> Result<i32, ParseIntError> {
@@ -9,12 +19,42 @@ fn parse_str(input: &str) -> Result<i32, ParseIntError> {
         .parse::<i32>()?; // Add a ? each time to check and pass it on
     Ok(parsed_number)
 }
-
-fn main() {
-    let str_vec = vec!["Seven", "8", "9.0", "nice", "6060"];
-
-    for item in str_vec {
-        let parsed = parse_str(item);
-        println!("{:?}", parsed);
+fn prints_three_things(vector: Vec<i32>) {
+    if vector.len() != 3 {
+        panic!("my_vec must always have three items") // will panic if the length is not 3
     }
+    println!("{}, {}, {}", vector[0], vector[1], vector[2]);
+}
+fn main() {
+    // let str_vec = vec!["Seven", "8", "9.0", "nice", "6060"];
+
+    // for item in str_vec {
+    //     let parsed = parse_str(item);
+    //     println!("{:?}", parsed);
+    // }
+
+    // let my_vec = vec![8, 9, 10, 10, 55, 99];
+    // prints_three_things(my_vec);
+
+    // let my_name = "Loki Laufeyson";
+
+    // assert!(my_name == "Loki Laufeyson");
+    // assert_eq!(my_name, "Loki Laufeyson");
+    // assert_ne!(my_name, "Mithridates");
+    // // These messages will only display if the program panics. So if you run this:
+    // assert!(
+    //     my_name == "Loki Laufeyson",
+    //     "{} should be Loki Laufeyson",
+    //     my_name
+    // );
+    // assert_eq!(
+    //     my_name, "Loki Laufeyson",
+    //     "{} and Loki Laufeyson should be equal",
+    //     my_name
+    // );
+    // assert_ne!(
+    //     my_name, "Loki Laufeyson",
+    //     "You entered {}. Input must not equal Loki Laufeyson",
+    //     my_name
+    // );
 }
