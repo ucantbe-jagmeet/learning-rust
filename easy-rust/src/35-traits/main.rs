536f6f77446b2783c8b35e6f5a1c3b1c2a6a6648
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-24 23:04:38 +0530

    traits

--- easy-rust/src/35-traits/main.rs
+++ easy-rust/src/35-traits/main.rs
@@ -34,3 +34,47 @@ fn main() {
     rover.run();
 }
 */
+/*
+#[derive(Debug)]
+struct Cat {
+    name: String,
+    age: u8,
+}
+
+fn main() {
+    let mr_mantle = Cat {
+        name: "Reggie Mantle".to_string(),
+        age: 4,
+    };
+
+    println!("Mr. Mantle is a {:?}", mr_mantle)
+}
+*/
+
+use std::fmt;
+
+struct Cat {
+    name: String,
+    age: u8,
+}
+
+impl fmt::Display for Cat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is a cat who is {} years old.", self.name, self.age)
+    }
+}
+fn print_cats(pet: String) {
+    println!("{}", pet);
+}
+
+fn main() {
+    let mr_mantle = Cat {
+        name: "Reggie Mantle".to_string(),
+        age: 4,
+    };
+    print_cats(mr_mantle.to_string());
+    println!(
+        "Mr. mantle's string is {} letters long",
+        mr_mantle.to_string().chars().count()
+    );
+}
