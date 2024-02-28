f0a99cdef7df6bb67fd60056bb5fd3843f122f67
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-28 23:05:34 +0530

    iterators

--- easy-rust/src/37-iterators/main.rs
+++ easy-rust/src/37-iterators/main.rs
@@ -5,6 +5,10 @@ An iterator is a construct that can give you the items in the collection, one at
 .iter_mut() for an iterator of mutable references
 .into_iter() for an iterator of values (not references)
 
+
+#### working
+
+An iterator works by using a method called .next(), which gives an Option. When you use an iterator, Rust calls next() over and over again. If it gets Some, it keeps going. If it gets None, it stops.
 */
 
 /*
@@ -23,12 +27,12 @@ fn main() {
 
 */
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Library {
     library_type: LibraryType,
     books: Vec<String>,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum LibraryType {
     // libraries can be city libraries or country libraries
     City,
@@ -47,6 +51,17 @@ impl Library {
     }
 }
 
+impl Iterator for Library {
+    type Item = String; // this is must to write type
+                        // This is the associated type. Our iterator will be for our list of books, which is a Vec<String>. When we call next, it will give us a String. So we will write type Item = String;. That is the associated item.
+    fn next(&mut self) -> Option<String> {
+        match self.books.pop() {
+            Some(book) => Some(book + " is found"), // rust allows to add String and &str
+            None => None,
+        }
+    }
+}
+
 fn main() {
     let mut my_library = Library::new(); // make a new library
     my_library.add_book("The Doom of the Darksword"); // add some books
@@ -54,5 +69,10 @@ fn main() {
     my_library.add_book("구운몽");
     my_library.add_book("吾輩は猫である");
 
-    println!("{:?}", my_library.books); // we can print our list of books
+    // println!("{:?}", my_library.books); // we can print our list of books
+
+    for item in my_library.clone() {
+        // we can use a for loop now. Give it a clone so Library won't be destroyed
+        println!("{}", item);
+    }
 }
