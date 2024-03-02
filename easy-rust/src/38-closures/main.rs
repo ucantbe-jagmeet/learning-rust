e0ffb766c529207a0fef2f96f0211642fbe61c30
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-02 23:35:33 +0530

    closures

---
+++ easy-rust/src/38-closures/main.rs
@@ -0,0 +1,6 @@
+// Closures are like quick functions that don't need a name. Sometimes they are called lambdas
+
+fn main() {
+    let my_closure = || println!("This is a closure");
+    my_closure();
+}
