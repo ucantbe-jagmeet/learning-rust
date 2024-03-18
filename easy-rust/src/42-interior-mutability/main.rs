b4c930f438eaef0a8b4345bb518f32522bb7fca8
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-18 22:00:05 +0530

    interior mutatbility

--- easy-rust/src/42-interior-mutability/main.rs
+++ easy-rust/src/42-interior-mutability/main.rs
@@ -1,7 +1,15 @@
 //Rust has many ways to allow some safe mutability inside of something that is immutable. The most simple way is called Cell.
+//A RefCell is another way to change values without needing to declare mut. It means "reference cell", and is like a Cell but uses references instead of copies.
+use std::cell::{Cell, RefCell};
 
-use std::cell::Cell;
-
+#[derive(Debug)]
+struct User {
+    id: u32,
+    year_registered: u32,
+    username: String,
+    active: RefCell<bool>,
+    // Many other fields
+}
 struct PhoneModel {
     company_name: String,
     model_name: String,
@@ -23,4 +31,13 @@ fn main() {
 
     // 10 years later, super_phone_3000 is not on sale anymore
     super_phone_3000.on_sale.set(false);
+
+    let user_1 = User {
+        id: 1,
+        year_registered: 2020,
+        username: "User 1".to_string(),
+        active: RefCell::new(true),
+    };
+
+    println!("{:?}", user_1.active);
 }
