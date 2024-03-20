dacc349abfba7800366e29665cb701d92e913793
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-20 23:44:10 +0530

    mutex

--- easy-rust/src/42-interior-mutability/main.rs
+++ easy-rust/src/42-interior-mutability/main.rs
@@ -1,6 +1,7 @@
 //Rust has many ways to allow some safe mutability inside of something that is immutable. The most simple way is called Cell.
 //A RefCell is another way to change values without needing to declare mut. It means "reference cell", and is like a Cell but uses references instead of copies.
 use std::cell::{Cell, RefCell};
+use std::sync::Mutex;
 
 #[derive(Debug)]
 struct User {
@@ -20,24 +21,52 @@ struct PhoneModel {
 }
 
 fn main() {
-    let super_phone_3000 = PhoneModel {
-        company_name: "YY Electronics".to_string(),
-        model_name: "Super Phone 3000".to_string(),
-        screen_size: 7.5,
-        memory: 4_000_000,
-        date_issued: 2020,
-        on_sale: Cell::new(true),
-    };
+    /*
+      let super_phone_3000 = PhoneModel {
+         company_name: "YY Electronics".to_string(),
+         model_name: "Super Phone 3000".to_string(),
+         screen_size: 7.5,
+         memory: 4_000_000,
+         date_issued: 2020,
+         on_sale: Cell::new(true),
+     };
 
-    // 10 years later, super_phone_3000 is not on sale anymore
-    super_phone_3000.on_sale.set(false);
+     // 10 years later, super_phone_3000 is not on sale anymore
+     super_phone_3000.on_sale.set(false);
 
-    let user_1 = User {
-        id: 1,
-        year_registered: 2020,
-        username: "User 1".to_string(),
-        active: RefCell::new(true),
-    };
+     let user_1 = User {
+         id: 1,
+         year_registered: 2020,
+         username: "User 1".to_string(),
+         active: RefCell::new(true),
+     };
 
-    println!("{:?}", user_1.active);
+     println!("{:?}", user_1.active);
+    */
+
+    /*
+        There are many methods for RefCell. Two of them are .borrow() and .borrow_mut(). With these methods, you can do the same thing you do with & and &mut. The rules are the same:
+
+    Many borrows is fine,
+    one mutable borrow is fine,
+    but mutable and immutable together is not fine.
+
+         */
+
+    //######## Mutex ##############
+    let my_mutex = Mutex::new(5); // A new Mutex<i32>. We don't need to say mut
+    let mut mutex_changer = my_mutex.lock().unwrap(); // mutex_changer is a MutexGuard
+                                                      // It has to be mut because we will change it
+                                                      // Now it has access to the Mutex
+                                                      // Let's print my_mutex to see:
+
+    println!("{:?}", my_mutex); // This prints "Mutex { data: <locked> }"
+                                // So we can't access the data with my_mutex now,
+                                // only with mutex_changer
+
+    println!("{:?}", mutex_changer); // This prints 5. Let's change it to 6.
+
+    *mutex_changer = 6; // mutex_changer is a MutexGuard<i32> so we use * to change the i32
+
+    println!("{:?}", mutex_changer);
 }
