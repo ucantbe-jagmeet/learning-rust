79f4967baad370accf36dc92ef13593ecf86c4e9
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-27 19:46:03 +0530

    read-csv file

--- 50-projects-akhil-sharma/read-csv-file/src/main.rs
+++ 50-projects-akhil-sharma/read-csv-file/src/main.rs
@@ -1,7 +1,16 @@
-use csv;
 use std::error::Error;
 
-fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {}
+use csv;
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
 
 fn main() {
     if let Err(e) = read_from_file("../customers.csv") {
