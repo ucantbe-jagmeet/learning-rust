6097d57a368979397bde71f2476af9420770c07e
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-27 21:11:38 +0530

    RwLock

--- easy-rust/src/42-interior-mutability/main.rs
+++ easy-rust/src/42-interior-mutability/main.rs
@@ -106,4 +106,21 @@ fn main() {
 
     let write1 = my_rwlock.write().unwrap(); // uh oh, now the program will wait forever
     */
+
+    //RwLock
+
+    let my_rwlock = RwLock::new(5);
+
+    let read1 = my_rwlock.read().unwrap();
+    let read2 = my_rwlock.read().unwrap();
+
+    println!("{:?}, {:?}", read1, read2);
+
+    drop(read1);
+    drop(read2); // we dropped both, so we can use .write() now
+
+    let mut write1 = my_rwlock.write().unwrap();
+    *write1 = 6;
+    drop(write1);
+    println!("{:?}", my_rwlock);
 }
