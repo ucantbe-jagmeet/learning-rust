c455b8596184c40bc22dad08e2412a50db7adeb0
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-28 20:25:03 +0530

    impl trait

---
+++ easy-rust/src/49-impl-trait/main.rs
@@ -0,0 +1,80 @@
+//impl Trait is similar to generics. impl Trait, which is similar. Instead of a type T, we can bring in a type impl Trait. Then it will take in a type that implements that trait. It is almost the same
+use std::fmt::Display;
+
+/*
+fn gives_higher_i32<T: PartialOrd + Display>(one: T, two: T) {
+    let higher = if one > two { one } else { two };
+    println!("{} is higher.", higher);
+}
+
+fn print_it(input: impl Into<String> + Display) {
+    // Takes anything that can turn into a String and has Display
+    println!("you can print many things, including {}", input);
+}
+
+fn main() {
+    // gives_higher_i32(8, 10);
+    let name = "Tuon";
+    let string_name = String::from("Tuon");
+    print_it(name);
+    print_it(string_name);
+}
+ */
+
+enum TimeOfDay {
+    // just a simple enum
+    Dawn,
+    Day,
+    Sunset,
+    Night,
+}
+
+fn change_fear(input: TimeOfDay) -> impl FnMut(f64) -> f64 {
+    // The function takes a TimeOfDay. It returns a closure.
+    // We use impl FnMut(64) -> f64 to say that it needs to
+    // change the value, and also gives the same type back.
+    use TimeOfDay::*; // So we only have to write Dawn, Day, Sunset, Night
+                      // Instead of TimeOfDay::Dawn, TimeOfDay::Day, etc.
+    match input {
+        Dawn => |x| {
+            // This is the variable character_fear that we give it later
+            println!(
+                "The morning sun has vanquished the horrible night. You no longer feel afraid."
+            );
+            println!("Your fear is now {}", x * 0.5);
+            x * 0.5
+        },
+        Day => |x| {
+            println!("What a nice day. Maybe put your feet up and rest a bit.");
+            println!("Your fear is now {}", x * 0.2);
+            x * 0.2
+        },
+        Sunset => |x| {
+            println!("The sun is almost down! This is no good.");
+            println!("Your fear is now {}", x * 1.4);
+            x * 1.4
+        },
+        Night => |x| {
+            println!("What a horrible night to have a curse.");
+            println!("Your fear is now {}", x * 5.0);
+            x * 5.0
+        },
+    }
+}
+
+fn main() {
+    use TimeOfDay::*;
+    let mut character_fear = 10.0; // Start Simon with 10
+
+    let mut daytime = change_fear(Day); // Make four closures here to call every time we want to change Simon's fear.
+    let mut sunset = change_fear(Sunset);
+    let mut night = change_fear(Night);
+    let mut morning = change_fear(Dawn);
+
+    character_fear = daytime(character_fear); // Call the closures on Simon's fear. They give a message and change the fear number.
+                                              // In real life we would have a Character struct and use it as a method instead,
+                                              // like this: character_fear.daytime()
+    character_fear = sunset(character_fear);
+    character_fear = night(character_fear);
+    character_fear = morning(character_fear);
+}
