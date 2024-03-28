d9b4c2cd7e64771a7a8daeb0a9fc3cd30c5727d4
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-29 00:37:04 +0530

    channels

--- easy-rust/src/51-channels/main.rs
+++ easy-rust/src/51-channels/main.rs
@@ -1,1 +1,12 @@
 //  channel is an easy way to use many threads that send to one place. They are fairly popular because they are pretty simple to put together. You can create a channel in Rust with std::sync::mpsc. mpsc means "multiple producer, single consumer", so "many threads sending to one place". To start a channel, you use channel(). This creates a Sender and a Receiver that are tied together.
+
+use std::sync::mpsc::{channel, Receiver, Sender};
+
+fn main() {
+    /*
+     let (sender, receiver): (Sender<i32>, Receiver<i32>) = channel();
+
+    sender.send(5).unwrap();
+    println!("{}", receiver.recv().unwrap());
+     */
+}
