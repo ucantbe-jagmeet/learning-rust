ba622b17cef3bb21d42c5f7b8af6e3f3d77a51a1
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-03-30 00:51:25 +0530

    testing

--- easy-rust/src/51-channels/main.rs
+++ easy-rust/src/51-channels/main.rs
@@ -4,9 +4,44 @@ use std::sync::mpsc::{channel, Receiver, Sender};
 
 fn main() {
     /*
-     let (sender, receiver): (Sender<i32>, Receiver<i32>) = channel();
+    let (sender, receiver): (Sender<i32>, Receiver<i32>) = channel();
 
     sender.send(5).unwrap();
     println!("{}", receiver.recv().unwrap());
      */
+
+    /*
+    let (sender, receiver) = channel();
+    let sender_clone = sender.clone();
+
+    std::thread::spawn(move || {
+        // move sender in
+        sender.send("Send a &str this time").unwrap();
+    });
+
+    std::thread::spawn(move || {
+        // move sender_clone in
+        sender_clone.send("And here is another &str").unwrap();
+    });
+
+    println!("{}", receiver.recv().unwrap());
+    */
+
+    // The two threads start sending, and then we println!. It might say Send a &str this time or And here is another &str, depending on which thread finished first. Let's make a join handle to make them wait.
+
+    let (sender, receiver) = channel();
+    let sender_clone = sender.clone();
+    let mut handle_vec = vec![];
+
+    handle_vec.push(std::thread::spawn(move || {
+        sender.send("Send a &str this time").unwrap();
+    }));
+
+    handle_vec.push(std::thread::spawn(move || {
+        sender.send("And here is the anther &str").unwrap();
+    }));
+
+    for _ in handle_vec {
+        println!("{:?}", receiver.recv().unwrap());
+    }
 }
