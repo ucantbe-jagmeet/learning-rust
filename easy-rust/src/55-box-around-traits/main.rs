40db558f306005c17ae135b111d5014514d13ca9
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-01 00:20:46 +0530

    box around traits

--- easy-rust/src/55-box-around-traits/main.rs
+++ easy-rust/src/55-box-around-traits/main.rs
@@ -26,23 +26,53 @@ impl fmt::Display for ErrorTwo {
 }
 
 // Make a function that just returns a String or an error
-fn returns_errors(input: u8) -> Result<String, Box<dyn Error>> {
-    // With Box<dyn Error> you can return anything that has the Error trait
+#![allow(dead_code)] // Tell the compiler to be quiet
+use std::mem::size_of; // This gives the size of a type
 
-    match input {
-        0 => Err(Box::new(ErrorOne)), // Don't forget to put it in a box
-        1 => Err(Box::new(ErrorTwo)),
-        _ => Ok("Looks fine to me".to_string()), // This is the success type
-    }
+trait JustATrait {} // We will implement this on everything
+
+enum EnumOfNumbers {
+    I8(i8),
+    AnotherI8(i8),
+    OneMoreI8(i8),
 }
+impl JustATrait for EnumOfNumbers {}
+
+struct StructOfNumbers {
+    an_i8: i8,
+    another_i8: i8,
+    one_more_i8: i8,
+}
+impl JustATrait for StructOfNumbers {}
+
+enum EnumOfOtherTypes {
+    I8(i8),
+    AnotherI8(i8),
+    Collection(Vec<String>),
+}
+impl JustATrait for EnumOfOtherTypes {}
+
+struct StructOfOtherTypes {
+    an_i8: i8,
+    another_i8: i8,
+    a_collection: Vec<String>,
+}
+impl JustATrait for StructOfOtherTypes {}
+
+struct ArrayAndI8 {
+    array: [i8; 1000], // This one will be very large
+    an_i8: i8,
+    in_u8: u8,
+}
+impl JustATrait for ArrayAndI8 {}
 
 fn main() {
-    let vec_of_u8s = vec![0_u8, 1, 80]; // Three numbers to try out
-
-    for number in vec_of_u8s {
-        match returns_errors(number) {
-            Ok(input) => println!("{}", input),
-            Err(message) => println!("{}", message),
-        }
-    }
+    println!(
+        "{}, {}, {}, {}, {}",
+        size_of::<EnumOfNumbers>(),
+        size_of::<StructOfNumbers>(),
+        size_of::<EnumOfOtherTypes>(),
+        size_of::<StructOfOtherTypes>(),
+        size_of::<ArrayAndI8>(),
+    );
 }
