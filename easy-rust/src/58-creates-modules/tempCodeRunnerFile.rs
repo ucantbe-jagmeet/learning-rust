35be90c3d5cbe719eb2e5e6f21abefe440fc2605
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-04 02:24:53 +0530

    crates and modules

---
+++ easy-rust/src/58-creates-modules/tempCodeRunnerFile.rs
@@ -0,0 +1,8 @@
+mod print_things {
+//     use std::fmt::Display;
+
+//     pub fn prints_one_thing<T: Display>(input: T) {
+//         // Print anything that implements Display
+//         println!("{}", input)
+//     }
+// }
