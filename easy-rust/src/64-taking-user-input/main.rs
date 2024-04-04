943e5dcbda336741c611fd6b709795c3863897dd
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-05 01:09:02 +0530

    taking user input

--- easy-rust/src/64-taking-user-input/main.rs
+++ easy-rust/src/64-taking-user-input/main.rs
@@ -1,14 +1,46 @@
+use std::env::args;
+use std::env::args;
 use std::io;
 
-fn main() {
-    println!("Please type something, or x to excape:");
-    let mut input_string = String::new();
+enum Letters {
+    Capitalize,
+    Lowercase,
+    Nothing,
+}
 
-    while input_string.trim() != "x" {
-        input_string.clear();
-        io::stdin().read_line(&mut input_string).unwrap();
-        println!("you wrote {}", input_string);
-    }
+fn main() {
+    // println!("Please type something, or x to excape:");
+    // let mut input_string = String::new();
+
+    // while input_string.trim() != "x" {
+    //     input_string.clear();
+    //     io::stdin().read_line(&mut input_string).unwrap();
+    //     println!("you wrote {}", input_string);
+    // }
     // loop will not exited because , This is because the keyboard input is actually not just something, it is something and the Enter key. There is an easy method to fix this called .trim(), which removes all the whitespace.
-    println!("see you later");
+    // println!("see you later");
+
+    // let input = args();
+    // for entry in input {
+    //     println!("You entered: {}", entry);
+    // }
+
+    let mut changes = Letters::Nothing;
+    let input = args().collect::<Vec<_>>();
+
+    if input.len() > 2 {
+        match input[1].as_str() {
+            "capital" => changes = Letters::Capitalize,
+            "lowercase" => changes = Letters::Lowercase,
+            _ => {}
+        }
+    }
+
+    for word in input.iter().skip(2) {
+        match changes {
+            Letters::Capitalize => println!("{}", word.to_uppercase()),
+            Letters::Lowercase => println!("{}", word.to_lowercase()),
+            _ => println!("{}", word),
+        }
+    }
 }
