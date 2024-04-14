4e48b4f1814a341c12ce1098ad782703b804b1dc
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-14 13:53:05 +0530

    vectors

--- easy-rust-02/src/22-vector/main.rs
+++ easy-rust-02/src/22-vector/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    let name1 = String::from("Windy");
-    let name2 = String::from("Gomesy");
+    let vec_of_ten = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // Everything is the same as above except we added vec!.
+    let three_to_five = &vec_of_ten[2..5];
+    let start_at_two = &vec_of_ten[1..];
+    let end_at_five = &vec_of_ten[..5];
+    let everything = &vec_of_ten[..];
 
-    let mut my_vec = Vec::new();
-
-    my_vec.push(name1); // Now it knows: it's Vec<String>
-    my_vec.push(name2);
+    println!(
+        "Three to five: {:?},
+start at two: {:?}
+end at five: {:?}
+everything: {:?}",
+        three_to_five, start_at_two, end_at_five, everything
+    );
 }
