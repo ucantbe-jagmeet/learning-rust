05e6d149b2fe1864e0eef4369d46b6ecc89fdb73
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-14 13:54:22 +0530

    vectors

--- easy-rust-02/src/22-vector/main.rs
+++ easy-rust-02/src/22-vector/main.rs
@@ -1,4 +1,16 @@
 fn main() {
+    let name1 = String::from("Windy");
+    let name2 = String::from("Gomesy");
+
+    let mut my_vec = Vec::new();
+    // If we run the program now, the compiler will give an error.
+    // It doesn't know the type of vec.
+
+    my_vec.push(name1); // Now it knows: it's Vec<String>
+    my_vec.push(name2);
+
+    let mut my_vec1: Vec<String> = Vec::new(); // The compiler knows the type
+                                               // so there is no error.
     let vec_of_ten = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     // Everything is the same as above except we added vec!.
     let three_to_five = &vec_of_ten[2..5];
@@ -7,10 +19,38 @@ fn main() {
     let everything = &vec_of_ten[..];
 
     println!(
-        "Three to five: {:?},
-start at two: {:?}
-end at five: {:?}
-everything: {:?}",
+        "Three to five: {:?},start at two: {:?} end at five: {:?} everything: {:?}",
         three_to_five, start_at_two, end_at_five, everything
     );
+
+    let mut num_vec = Vec::new();
+    println!("{}", num_vec.capacity()); // 0 elements: prints 0
+    num_vec.push('a'); // add one character
+    println!("{}", num_vec.capacity()); // 1 element: prints 4. Vecs with 1 item always start with capacity 4
+    num_vec.push('a'); // add one more
+    num_vec.push('a'); // add one more
+    num_vec.push('a'); // add one more
+    println!("{}", num_vec.capacity()); // 4 elements: still prints 4.
+    num_vec.push('a'); // add one more
+    println!("{}", num_vec.capacity()); // prints 8. We have 5 elements, but it doubled 4 to 8 to make space
+
+    /*
+         let mut num_vec = Vec::with_capacity(8); // Give it capacity 8
+    num_vec.push('a'); // add one character
+    println!("{}", num_vec.capacity()); // prints 8
+    num_vec.push('a'); // add one more
+    println!("{}", num_vec.capacity()); // prints 8
+    num_vec.push('a'); // add one more
+    println!("{}", num_vec.capacity()); // prints 8.
+    num_vec.push('a'); // add one more
+    num_vec.push('a'); // add one more // Now we have 5 elements
+    println!("{}", num_vec.capacity()); // Still 8
+
+     */
+
+    let my_vec: Vec<u8> = [1, 2, 3].into();
+    let my_vec2: Vec<_> = [9, 0, 10].into(); // Vec<_> means "choose the Vec type for me"
+                                             // Rust will choose Vec<i32>
+
+    //  You remember that you can use .into() to make a &str into a String. You can also use it to make an array into a Vec. You have to tell .into() that you want a Vec, but you don't have to choose the type of Vec. If you don't want to choose, you can write Vec<_>
 }
