e70dea5e4f664b2a0565487bef7bea017c9b4b7f
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-15 13:59:36 +0530

    Control flow

---
+++ easy-rust-02/src/24-control-flow/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let my_number = 5;
+    if my_number == 7 {
+        println!("It's seven");
+    } else if my_number == 6 {
+        println!("It's six")
+    } else {
+        println!("It's a different number")
+    }
+}
