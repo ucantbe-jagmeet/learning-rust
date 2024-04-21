31a4f62b9fb9f734fb5f4721ea92cbc1f6f71be1
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-21 15:19:24 +0530

    ref dot operator

---
+++ easy-rust-02/src/30-ref-dot-operator/main.rs
@@ -0,0 +1,38 @@
+// references and dot operators
+
+struct Item {
+    number: u8,
+}
+
+impl Item {
+    fn compare_number(&self, other_number: u8) {
+        println!(
+            "are {} and {} equal? {}",
+            self.number,
+            other_number,
+            self.number == other_number
+        );
+    }
+}
+fn main() {
+    let my_number = 8;
+    let reference_number = &my_number;
+
+    // println!("The value of my_number is {}", my_number);
+    // println!("{}", my_number == reference_number);
+    // So we change line 5 to println!("{}", my_number == *reference); and now it prints true because it's now i32 == i32, not i32 == &i32. This is called dereferencing.
+
+    let item = Item { number: 8 };
+
+    // let ref_number = &item.number;
+    // let ref_item = &item;
+    // println!("{}", ref_number == 10); // &u8 and u8 cannot be compared
+    // println!("{}", ref_item.number);
+
+    let reference_item = &item; // This is type &Item
+    let reference_item_two = &reference_item; // This is type &&Item
+
+    item.compare_number(8); // the method works
+    reference_item.compare_number(8); // it works here too
+    reference_item_two.compare_number(8); // and here
+}
