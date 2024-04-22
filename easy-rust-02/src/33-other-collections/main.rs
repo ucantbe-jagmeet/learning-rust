a31be6c4aed99f9c46f9ff4c954b09565e20c6db
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-22 20:28:20 +0530

    collection HashMap

--- easy-rust-02/src/33-other-collections/main.rs
+++ easy-rust-02/src/33-other-collections/main.rs
@@ -6,19 +6,36 @@ struct City {
 }
 
 fn main() {
-    let mut tallin = City {
-        name: "Tallinn".to_string(),
-        population: HashMap::new(),
-    };
+    // let mut tallin = City {
+    //     name: "Tallinn".to_string(),
+    //     population: HashMap::new(),
+    // };
 
-    tallin.population.insert(1372, 3_250);
-    tallin.population.insert(1851, 24_000);
-    tallin.population.insert(2020, 437_619);
+    // tallin.population.insert(1372, 3_250);
+    // tallin.population.insert(1851, 24_000);
+    // tallin.population.insert(2020, 437_619);
 
-    for (year, population) in tallin.population {
-        println!(
-            "In the year {} the city of {} had a population of {}.",
-            year, tallin.name, population
-        );
+    // for (year, population) in tallin.population {
+    //     println!(
+    //         "In the year {} the city of {} had a population of {}.",
+    //         year, tallin.name, population
+    //     );
+    // }
+
+    let canadian_cities = vec!["calgary", "vancouver", "Gimli"];
+    let german_cities = vec!["Karlsruhe", "Bad Doberan", "Bielefeld"];
+
+    let mut city_hashmap = HashMap::new();
+
+    for city in canadian_cities {
+        city_hashmap.insert(city, "Canada");
     }
+
+    for city in german_cities {
+        city_hashmap.insert(city, "German");
+    }
+
+    println!("{:?}", city_hashmap["Bielefeld"]);
+    println!("{:?}", city_hashmap.get("Bielefeld"));
+    println!("{:?}", city_hashmap.get("Bielefeldd"));
 }
