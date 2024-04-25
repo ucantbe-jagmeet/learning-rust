2fe0e308c4974035cbd1e6abcd410eed5f98bb4e
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-26 02:44:24 +0530

    Clone on write

---
+++ easy-rust-02/src/43-cow/main.rs
@@ -0,0 +1,1 @@
+// Cow is a very convenient enum. It means "clone on write" and lets you return a &str if you don't need a String, and a String if you need it. (It can also do the same with arrays vs. Vecs, etc.)
