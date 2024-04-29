829b8f3a84c7808ad104b34ee11c8487a4a0e835
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-04-29 17:55:19 +0530

    defaut and the builder pattern

---
+++ easy-rust-02/src/56-default-builder-pattern/main.rs
@@ -0,0 +1,91 @@
+// You can implement the Default trait to give values to a struct or enum that you think will be most common. The builder pattern works nicely with this to let users easily make changes when they want. First let's look at Default. Actually, most general types in Rust already have Default. They are not surprising: 0, "" (empty strings), false, etc.
+
+#[derive(Debug)]
+struct Character {
+    name: String,
+    age: u8,
+    height: u32,
+    weight: u32,
+    lifestate: LifeState,
+    can_use: bool, // Here is the new value
+}
+
+#[derive(Debug)]
+enum LifeState {
+    Alive,
+    Dead,
+    NeverAlive,
+    Uncertain,
+}
+
+impl Character {
+    fn new() -> Self {
+        Self {
+            name: "Billy".to_string(),
+            age: 15,
+            height: 170,
+            weight: 70,
+            lifestate: LifeState::Alive,
+            can_use: true, // .new() makes a fine character, so it is true
+        }
+    }
+
+    fn height(mut self, height: u32) -> Self {
+        self.height = height;
+        self.can_use = false; // Now the user can't use the character
+        self
+    }
+
+    fn weight(mut self, weight: u32) -> Self {
+        self.weight = weight;
+        self.can_use = false;
+        self
+    }
+
+    fn name(mut self, name: &str) -> Self {
+        self.name = name.to_string();
+        self.can_use = false;
+        self
+    }
+
+    fn build(mut self) -> Result<Character, String> {
+        if self.height < 200 && self.weight < 300 && !self.name.to_lowercase().contains("smurf") {
+            self.can_use = true; // Everything is okay, so set to true
+            Ok(self) // and return the character
+        } else {
+            Err("Could not create character. Characters must have:
+1) Height below 200
+2) Weight below 300
+3) A name that is not Smurf (that is a bad word)"
+                .to_string())
+        }
+    }
+}
+
+fn main() {
+    let character_with_smurf = Character::new().name("Lol I am Smurf!!").build(); // This one contains "smurf" - not okay
+    let character_too_tall = Character::new().height(400).build(); // Too tall - not okay
+    let character_too_heavy = Character::new().weight(500).build(); // Too heavy - not okay
+    let okay_character = Character::new()
+        .name("Billybrobby")
+        .height(180)
+        .weight(100)
+        .build(); // This character is okay. Name is fine, height and weight are fine
+
+    // Now they are not Character, they are Result<Character, String>. So let's put them in a Vec so we can see them:
+    let character_vec = vec![
+        character_with_smurf,
+        character_too_tall,
+        character_too_heavy,
+        okay_character,
+    ];
+
+    for character in character_vec {
+        // Now we will print the character if it's Ok, and print the error if it's Err
+        match character {
+            Ok(character_info) => println!("{:?}", character_info),
+            Err(err_info) => println!("{}", err_info),
+        }
+        println!(); // Then add one more line
+    }
+}
