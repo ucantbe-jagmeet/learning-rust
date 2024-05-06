a7b2f70ba7b54cf32f37a4e4daa744c5e5281a94
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-05-06 21:39:53 +0530

    stack heap

--- hkirat/src/stack-heap/main.rs
+++ hkirat/src/stack-heap/main.rs
@@ -8,6 +8,7 @@
 
 fn main() {
     stack_fn(); // call the function that uses the stack memory
+    heap_fn(); // call the function that uses the heap memory
 }
 
 fn stack_fn() {
@@ -16,4 +17,11 @@ fn stack_fn() {
     let c = a + b;
 
     println!("Stack function: sum is {}", c);
+}
+fn heap_fn() {
+    let s1 = String::from("Hello");
+    let s2 = String::from("World");
+    let combined = format!("{} {}", s1, s2);
+
+    println!("heap function: Combined string is {}", combined);
 }
