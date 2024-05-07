51352021ab2f63a8632b253c4b3018f2c49a7e15
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-05-08 05:14:14 +0530

    ownership with heap variable example 1

--- hkirat/src/ownership/main.rs
+++ hkirat/src/ownership/main.rs
@@ -5,23 +5,42 @@
 //ownership meant more for heap features
 
 fn main() {
-    let x = 1; // created on stack, owner is main fn
-    let y = 4; // created on stack, owner is main fn
+    // ##### STack Variable ###########
+    // let x = 1; // created on stack, owner is main fn
+    // let y = 4; // created on stack, owner is main fn
 
-    println!("{}", sum(x, y));
-    println!("Hello, World");
+    // println!("{}", sum(x, y));
+    // println!("Hello, World");
 
     /*
     let x = 1;
     {
         let y = 4 // store on stack
         // this is nothing to do with ownership
-
     }
 
     println!("{}", y); // error
 
     */
+
+    // ######### Heap Variable ###########
+
+    let s1 = String::from("Hello");
+    let s2 = s1; // here s2 doesn't point the copy of Hello , This means S2 also points to s1 variable
+                 // creating/copying in the heap is very expensive
+                 // two variables pointing to same string , if one them is die , then string will also die
+                 // so ownersip moved from s1 to s2 , and s1 is no longer owning that string
+
+    // println!("{}", s1);// no longer valid
+
+    create_str();
+}
+
+fn create_str() {
+    let s1 = String::from("Hello");
+    println!("{}", s1);
+    let s2 = s1;
+    println!("{}", s2);
 }
 
 fn sum(a: i32, b: i32) -> i32 {
