09c1119c1a0075bdec728f21ee6ab9430e121d02
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-05-08 05:24:56 +0530

    ownership with heap stack variable example 2

--- hkirat/src/ownership/main.rs
+++ hkirat/src/ownership/main.rs
@@ -25,15 +25,27 @@ fn main() {
 
     // ######### Heap Variable ###########
 
-    let s1 = String::from("Hello");
-    let s2 = s1; // here s2 doesn't point the copy of Hello , This means S2 also points to s1 variable
-                 // creating/copying in the heap is very expensive
-                 // two variables pointing to same string , if one them is die , then string will also die
-                 // so ownersip moved from s1 to s2 , and s1 is no longer owning that string
+    // let s1 = String::from("Hello");
+    // let s2 = s1; // here s2 doesn't point the copy of Hello , This means S2 also points to s1 variable
+    // creating/copying in the heap is very expensive
+    // two variables pointing to same string , if one them is die , then string will also die
+    // so ownersip moved from s1 to s2 , and s1 is no longer owning that string
 
     // println!("{}", s1);// no longer valid
 
-    create_str();
+    // create_str();
+
+    let my_string = String::from("hello");
+    // takes_ownership(my_string); // error
+    takes_ownership(my_string.clone()); // if we clone the string , it means it creating another string on heap, which in an expensive task
+
+    println!("{}", my_string);
+
+    // if lets assume my_String is a number  i32 , then it will create another copy on the stack , but here in the case of string it is different
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
 }
 
 fn create_str() {
