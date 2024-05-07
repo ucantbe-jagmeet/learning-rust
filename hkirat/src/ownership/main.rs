08fb69fbff5ed46ff5602712b4c8f2c9a165800d
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-05-08 05:27:19 +0530

    retaining ownership in rust

--- hkirat/src/ownership/main.rs
+++ hkirat/src/ownership/main.rs
@@ -35,17 +35,23 @@ fn main() {
 
     // create_str();
 
-    let my_string = String::from("hello");
+    // let my_string = String::from("hello");
     // takes_ownership(my_string); // error
-    takes_ownership(my_string.clone()); // if we clone the string , it means it creating another string on heap, which in an expensive task
+    // takes_ownership(my_string.clone()); // if we clone the string , it means it creating another string on heap, which in an expensive task
 
-    println!("{}", my_string);
+    // println!("{}", my_string);
 
     // if lets assume my_String is a number  i32 , then it will create another copy on the stack , but here in the case of string it is different
+
+    let mut my_string = String::from("Hello");
+    let my_string1 = takes_ownership(my_string);
+
+    println!("{}", my_string1)
 }
 
-fn takes_ownership(some_string: String) {
+fn takes_ownership(some_string: String) -> String {
     println!("{}", some_string);
+    return some_string;
 }
 
 fn create_str() {
