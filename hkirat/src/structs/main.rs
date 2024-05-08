32eba4e056cc04f39f0f3c8be0c48ec27258705d
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-05-08 23:07:42 +0530

    implementing structs

--- hkirat/src/structs/main.rs
+++ hkirat/src/structs/main.rs
@@ -5,6 +5,29 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+// impl Rect {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+
+impl std::fmt::Debug for Rect {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Rect {{ width: {}, height: {}, area: {} }}",
+            self.width,
+            self.height,
+            self.width * self.height
+        )
+    }
+}
+
 fn main() {
     let user1 = User {
         active: true,
@@ -13,5 +36,12 @@ fn main() {
         sign_in_count: 1,
     };
 
-    print!("User 1 username : {:?}", user1.username);
+    // print!("User 1 username : {:?}", user1.username);
+
+    let rect = Rect {
+        width: 30,
+        height: 23,
+    };
+
+    println!("Area of the rectangle: {:?}", rect);
 }
