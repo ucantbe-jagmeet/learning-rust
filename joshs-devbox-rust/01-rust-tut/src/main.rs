c399ca3bf5f7b341acfdfdffef98863240f1684f
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-05-13 03:52:11 +0530

    functions , mutability , arrays and slice

--- joshs-devbox-rust/01-rust-tut/src/main.rs
+++ joshs-devbox-rust/01-rust-tut/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
     // ##### printing to console #########
-    let a = 10;
-    let b = 15;
+    // let a = 10;
+    // let b = 15;
     // println!("{} {}", a, b);
     // ##### printing to console ends #########
 
     // ##### primitive variables #########
+    /*
     let unsigned: u8 = 10;
     let signed: i8 = -10;
     let float: f32 = 1.20;
@@ -18,9 +19,11 @@ fn main() {
 
     let is_true = true;
     // println!("isTrue:{}", is_true);
+    */
     // ##### primitive variables ends #########
 
     // ##### array, tuples #########
+    /*
     let arr: [u32; 3] = [10, 20, 30];
     let other_arr: [u32; 5] = [100; 5];
 
@@ -37,5 +40,39 @@ fn main() {
 
     let (a, b, c) = tuple;
     println!("value of b:  {}", b)
+    */
     // ##### array, tuples ends#########
+
+    //####### functions starts#######
+    // println!("is even {}", is_even(2));
+    //####### functions ends######
+
+    //####### mutability ######
+    /*
+    let mut num = 5;
+    num = 3;
+    println!("num :{}", num);
+    */
+    //####### mutability ends######
+
+    //#######arrays and slice ######
+    /*
+    let arr: [u8; 6] = [0, 1, 2, 3, 4, 5];
+    let slice = &arr[3..5];
+
+    borrowing_slice(arr, slice);
+    */
+    //#######arrays and slice ends ######
 }
+
+// fn is_even(num: u8) -> bool {
+//     let digit = num % 2;
+//     digit == 0
+// }
+
+// fn borrowing_slice(arr: [u8; 6], slice: &[u8]) {
+//     println!("{:?}", arr);
+//     println!("{:?}", slice);
+//     println!("length : {}", slice.len());
+//     println!("{} {}", slice[0], slice[1]);
+// }
