31852b0e27aadec3f1a7329be7d77cd267bfd52b
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-09-12 14:48:43 +0530

    isEven funtion

--- hk-rust-marathon/src/main.rs
+++ hk-rust-marathon/src/main.rs
@@ -1,3 +1,13 @@
+// q. write a function is_even that takes a number as an input and returs true if it is even
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", is_even(20))
+}
+
+
+fn is_even(num: i32) -> bool {
+    if num %2 ==0 {
+        return true
+    }
+    return  false;
 }
