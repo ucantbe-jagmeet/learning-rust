09917c4b3793e11fa28125593327a05259126a7d
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-09-16 13:08:39 +0530

    Custom Option

--- hk-rust-marathon/src/options.rs
+++ hk-rust-marathon/src/options.rs
@@ -1,17 +1,22 @@
-fn find_first_a(s:String) -> Option<i32> {
+enum CustomOption {
+    Some(i32),
+    None,
+}
+
+fn find_first_a(s:String) -> CustomOption {
     for(index, character) in s.chars().enumerate(){
         if character == 'a' {
-            return Some(index as i32);
+            return CustomOption::Some(index as i32);
         }
     }
-    return None
+    return CustomOption::None
 }
 
 fn main (){
     let my_string = String::from("rewewrrwerwaman");
 
     match find_first_a(my_string) {
-        Some(index) => println!("The letter is found at index {}", index),
-        None => println!("The letter is not found")
+        CustomOption::Some(index) => println!("The letter is found at index {}", index),
+        CustomOption::None => println!("The letter is not found")
     }
 }
